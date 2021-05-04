6bff42aed06b0191c0d01c6f3e04889d87147471
KatiaBravo <katia@alexbravo.com>
2021-05-04 15:21:33 -0700

    Add multiple screens

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,6 +1,10 @@
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
 import processing.core.*;
 
 public class Main extends PApplet{
@@ -8,6 +12,21 @@ public class Main extends PApplet{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("Main");
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+		window.setSize(800, 600);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		
+		
+		canvas.requestFocus();
 	}
 	
 	public void settings() {
