99f77b96c628a2ee74baebd43f12cbabf41f3f68
KatiaBravo <katia@alexbravo.com>
2021-05-14 09:52:36 -0700

    Fix the random code file

--- FinalProject/src/worldSetting/CodeWeMightNeed.java
+++ FinalProject/src/worldSetting/CodeWeMightNeed.java
@@ -7,42 +7,40 @@ package worldSetting;
  */
 public class CodeWeMightNeed {
 	
-//	elements = new Element[size][size];
-//
-//	for (int i = 0; i < size; i++) {
-//		for (int j = 0; j < size; j++) {
-//			float x = i * 5;
-//			float y = 0;
-//			float z = j * 5;
-//			elements[i][j] = new Element(x, y, z, 5);
-//			b.add(elements[i][j]);
-//		}
-//	}
-//	start = elements[0][0];
+	private void createElements(int size) {
+		Element[][] elements = new Element[size][size];
+		Element start;
 	
-//	private double random(double lower, double upper) {
-//	return Math.random() * (upper - lower) + lower;
-//}
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				float x = i * 5;
+				float y = 0;
+				float z = j * 5;
+				elements[i][j] = new Element(x, y, z, 5);
+//				b.add(elements[i][j]);
+			}
+		}
+		start = elements[0][0];
+	}
 	
-//	public void setPlayerAtStart(Camera player) {
-//	player.moveTo(start.getX(), start.getY()-15, start.getZ());
-//}
+	private double random(double lower, double upper) {
+		return Math.random() * (upper - lower) + lower;
+	}
 	
-//	might need, who knows
-//	public boolean isPointInCube(float x, float y, float z) {
-//		// the x y z coords of the block are in the center so +/- by size/2 in all
-//		// directions to get the edges
-//		float left = this.x - size / 2;
-//		float right = this.x + size / 2;
-//		float top = this.y - size / 2;
-//		float bottom = this.y + size / 2;
-//		float front = this.z - size / 2;
-//		float back = this.z + size / 2;
-//		if (x > left && x < right && y > top && y < bottom && z > front && z < back) {
-//			return true;
-//		}
-//
-//		return false;
-//	}
+	public boolean isPointInCube(float x, float y, float z, float size) {
+		// the x y z coords of the block are in the center so +/- by size/2 in all
+		// directions to get the edges
+		float left = x - size / 2;
+		float right = x + size / 2;
+		float top = y - size / 2;
+		float bottom = y + size / 2;
+		float front = z - size / 2;
+		float back = z + size / 2;
+		if (x > left && x < right && y > top && y < bottom && z > front && z < back) {
+			return true;
+		}
+
+		return false;
+	}
 
 }
