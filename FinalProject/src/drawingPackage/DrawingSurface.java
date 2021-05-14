23830336b1e3607ac5e064fc08578a4f37bde752
KatiaBravo <katia@alexbravo.com>
2021-05-14 16:14:55 -0700

    DrawingSurface javadoced, cleaned and added started position to the world

--- FinalProject/src/drawingPackage/DrawingSurface.java
+++ FinalProject/src/drawingPackage/DrawingSurface.java
@@ -4,6 +4,11 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import processing.core.*;
 
+/**
+ * Creates the screens for the main parts of the application
+ * @author Katia and Elise
+ *
+ */
 public class DrawingSurface extends PApplet{
 
 	public float ratioX, ratioY;
@@ -12,86 +17,89 @@ public class DrawingSurface extends PApplet{
 	private PortfolioScreen portfolioScreen;
 	private ArrayList<Screen> screens;
 
+	/**
+	 * Creates the different screens for different parts of the program
+	 * including worldScreen, portfolioScreen and whatever the beginning screen
+	 * when opening should be, in this case being the worldScreen
+	 */
 	public DrawingSurface() {
 		screens = new ArrayList<Screen>();
 
-		//0
-		worldScreen = new WorldScreen(this);
+		worldScreen = new WorldScreen(this); //0
 		screens.add(worldScreen);
 
-		//1
-		portfolioScreen = new PortfolioScreen();
+		portfolioScreen = new PortfolioScreen(); //1
 		screens.add(portfolioScreen);
 
-		//set activeScreen to world screen. first thing that shows is world screen
-		activeScreen = screens.get(0);
+		activeScreen = screens.get(0); //set activeScreen to world screen
 	}
 
+	/**
+	 * Sets the size of the screen for the 3D world
+	 */
 	public void settings() {
 		size(activeScreen.DRAWING_WIDTH, activeScreen.DRAWING_HEIGHT, processing.core.PConstants.P3D);
 	}
 
+	/**
+	 * Sets up the two screens, and if it is in the worldScreen
+	 * then it sets the position of the camera by calling setCameraAtStart()
+	 */
 	public void setup() {
 		surface.setResizable(true);
 		for (Screen s : screens)
 			s.setup();
-
-		//		worldScreen.setPlayerAtStart();
+		
+		if(activeScreen instanceof WorldScreen)
+			((WorldScreen) activeScreen).setCameraAtStart(this);
 	}
 
+	/**
+	 * Draws the active screen and sets up the ratio of the screen to draw
+	 */
 	public void draw() {
-		ratioX = (float)width/activeScreen.DRAWING_WIDTH;
-		ratioY = (float)height/activeScreen.DRAWING_HEIGHT;
+		ratioX = (float) width/activeScreen.DRAWING_WIDTH;
+		ratioY = (float) height/activeScreen.DRAWING_HEIGHT;
 
 		pushMatrix();
 		scale(ratioX, ratioY);
-
 		activeScreen.draw(this);
-
 		popMatrix();
-
 	}
 
+	/**
+	 * Calls the activeScreen's keyPressed()
+	 */
 	public void keyPressed() {
 		activeScreen.keyPressed(this);
 	}
 
-	// Removes key from array list
+	/**
+	 * Calls the activeScreen's keyReleased()
+	 */
 	public void keyReleased() {
 		activeScreen.keyReleased(this);
-
-
-		//		while (checkKey(keyCode))
-		//			keys.remove(new Integer(keyCode));
 	}
-
-	//
-	//	// Checks if given key code is in the array list
-	//	public boolean checkKey(int i) {
-	//		return keys.contains(i);
-	//	}
-
-	//	public void keyPressed() {
-	//		keys.add(keyCode);
-	//	}
-
-	//	public void keyReleased() {
-	//		while(keys.contains(keyCode))
-	//			keys.remove(new Integer(keyCode));
-	//	}
-
-	//	public boolean isPressed(Integer code) {
-	//		return keys.contains(code);
-	//	}
-	//	
+	
+	/**
+	 * Calls the activeScreen's mousePressed()
+	 */
 	public void mousePressed() {
 		activeScreen.mousePressed(this);
 	}
 
+	/**
+	 * Switches the active screen to what is passed in
+	 * @param i The index of the screen
+	 */
 	public void switchScreen(int i) {
 		activeScreen = screens.get(i);
 	}
 	
+	/**
+	 * Calls the activeScreen's mouseWheel()
+	 * @param event The mouse action that was done
+	 */
 	public void mouseWheel(MouseEvent event) {
 		activeScreen.mouseWheel(event);
 	}
